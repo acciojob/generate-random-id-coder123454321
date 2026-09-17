1672fb781660a3c4b85521284915795716964e53
coder123454321 <115930973+coder123454321@users.noreply.github.com>
2026-09-18 01:16:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function makeid(l) {
   // write your code here
 	let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 	let result="";
-	for(let i=0;i<=l;i++){
+	for(let i=0;i<l;i++){
 		let idx=Math.floor(Math.random()*chars.length);
 		result+=chars[idx];
 	}
